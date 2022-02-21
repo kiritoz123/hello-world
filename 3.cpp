f84f1ff1a59bf0f78f13d8db1409aa0959b3d0ac
Trần Quốc Huy <hn61078@gmail.com>
2022-02-21 22:12:58 +0700

    first commicct

---
+++ 3.cpp
@@ -0,0 +1,14 @@
+#include<cmath>
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=10000;
+    string s=to_string(a);
+    string c="1222.3";
+    double b=atof(c.c_str());
+    cout<<s.length();
+    cout<<b;
+    int **a = new int*[100] ;
+    
+}
